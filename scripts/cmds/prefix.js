ebaa6fe1b50b7b17a677640debb4d69db0f7e59b
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2025-10-27 13:36:04 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -13,19 +13,7 @@ category: "auto 🪐",
 onStart: async function(){}, 
 onChat: async function({ event, message, getLang }) {
 if (event.body && event.body.toLowerCase() === "prefix") {
-return message.reply({
-body: `
-‣ 𝐆𝐥𝐨𝐛𝐚𝐥 𝐩𝐫𝐞𝐟𝐢𝐱:.
-‣𝐘𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐩𝐫𝐞𝐟𝐢𝐱:.
-
-‣ 𝐀𝐝𝐦𝐢𝐧 
-‣ SAIF ఌ︎
-
-‣ 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 ⓕ
-محمد سيف الإسلام
-\n\n\n  `,
-attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/8ll3tv.mp4")
-});
+return message.reply({});
 }
 }
 }
